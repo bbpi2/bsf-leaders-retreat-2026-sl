d3fb34f0693462e2a154a735dd71ee5aefcbefe8
Brian Chan <brian.chan.p2@gmail.com>
2026-06-27 20:00:25 +1000

    added deploy page to action

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://bbpi2.github.io",
+	base: "/starlight-experimental",
 	integrations: [
 		starlight({
 			title: 'My Docs',
