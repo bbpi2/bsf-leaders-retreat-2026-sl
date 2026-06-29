23de1fdc09cfbcc43bd2ff45a8c718a7e2f0c340
Brian Chan <brian.chan.p2@gmail.com>
2026-06-29 20:41:39 +1000

    first cut bsf schedule & packlist

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,21 +8,8 @@ export default defineConfig({
 	base: "/bsf-leaders-retreat-2026-sl",
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
+			title: '',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/bbpi2' }],
 		}),
 	],
 });
